95759ce5869e32b15a4368884386df2e411eb6a1
Manuel Bugallo <marvinody@gmail.com>
2019-12-06 20:08:28 -0500

    chore: version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='mercari',
-    version='0.0.1',
+    version='0.0.2',
     author='marvinody',
     author_email='manny@sadpanda.moe',
     description='mercari api-like wrapper',
