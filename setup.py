ddd9a6584583a45e4d210e02313894b8494569a5
Manuel Bugallo <marvinody@gmail.com>
2023-07-11 10:32:22 -0400

    forgot to bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='mercari',
-    version='2.0.0',
+    version='2.0.1',
     author='marvinody',
     author_email='manny@sadpanda.moe',
     description='mercari api-like wrapper',
